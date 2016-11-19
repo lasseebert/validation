3fdfd0b431df8710f123f9fe76c4932396a44e86
certainty <certainty@lisp-unleashed.de>
2016-11-19 19:15:14 +0100

    Simplify rule tests

--- test/rule_test.exs
+++ test/rule_test.exs
@@ -5,6 +5,10 @@ defmodule Validation.RuleTest do
   alias Validation.Result
   alias Validation.Rule
 
+  def apply_rule(rule, data) do
+    Rule.apply(rule, %Result{data: data})
+  end
+
   test "building a simple custom rule" do
     val = fn result ->
       if result.data[:name] do
@@ -14,33 +18,24 @@ defmodule Validation.RuleTest do
       end
     end
 
-    rule = Rule.build(val, name: ["name must be filled"])
+    rule   = Rule.build(val, name: ["name must be filled"])
 
-    result = %Result{data: %{name: "Me"}} |> rule.compiled.()
-    assert result.errors == %{}
-
-    result = %Result{data: %{}} |> rule.compiled.()
-    assert result.errors == %{name: ["must be filled"]}
+    assert %{errors: %{}} = apply_rule(rule,  %{name: "Me"})
+    assert %{errors: %{name: ["must be filled"]}} = apply_rule(rule, %{})
   end
 
   test "built-in value rule" do
     filled? = Predicate.build_basic(fn value -> !(value in ["", nil]) end, "must be filled", "filled?")
     rule = Rule.built_in("value", :name, filled?)
 
-    result = %Result{data: %{name: "Me"}} |> rule.compiled.()
-    assert result.errors == %{}
-
-    result = %Result{data: %{}} |> rule.compiled.()
-    assert result.errors == %{name: ["must be filled"]}
+    assert %{errors: %{}} = apply_rule(rule, %{name: "Me"})
+    assert %{errors: %{name: ["must be filled"]}} = apply_rule(rule, %{})
   end
 
   test "built_in required rule" do
     rule = Rule.built_in("required", :name)
 
-    result = %Result{data: %{name: "Me"}} |> rule.compiled.()
-    assert result.errors == %{}
-
-    result = %Result{data: %{}} |> rule.compiled.()
-    assert result.errors == %{name: ["is missing"]}
+    assert %{errors: %{}} = apply_rule(rule, %{name: "Me"})
+    assert %{ errors: %{name: ["is missing"]}} = apply_rule(rule, %{})
   end
 end
