5804bf50e70a4d8adb6f8d7e443627d551b54ef0
certainty <certainty@lisp-unleashed.de>
2016-11-20 12:03:50 +0100

    Fallback to any for compilable

--- lib/validation/compilable.ex
+++ lib/validation/compilable.ex
@@ -1,5 +1,11 @@
 defprotocol Validation.Compilable do
+  @fallback_to_any true
   @type result       :: {:ok, any} | {:error, String.t}
   @spec compile(any) :: result
+
   def compile(compilable)
+end
+
+defimpl Validation.Compilable, for: Any do
+  def compile(_), do: {:error, "Can not compile #{__MODULE__}"}
 end
