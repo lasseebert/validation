f1f97d8929aadc9d4021ba0479b51619df1d4e6c
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2017-05-10 11:26:16 +0200

    Use fewer macros, 16

--- lib/validation/dsl.ex
+++ lib/validation/dsl.ex
@@ -7,27 +7,23 @@ defmodule Validation.DSL do
   Creates a schema from the AST specification
   """
   defmacro build_schema(do: rule_spec) do
+    rule_spec = Macro.escape(rule_spec)
     quote do
       rules = Validation.DSL.parse_rules(unquote(rule_spec))
       Validation.Schema.build(rules, %{})
     end
   end
 
-  defmacro parse_rules({:required, _, [field, predicate_spec]}) do
-    quote do
-      predicate = Validation.DSL.parse_predicate(unquote(predicate_spec))
-      field = unquote(field)
+  def parse_rules({:required, _, [field, predicate_spec]}) do
+    predicate = parse_predicate(predicate_spec)
 
-      value_rule = Validation.Rules.Value.build(field, predicate)
-      key_rule = Validation.Rules.RequiredKey.build(field)
+    value_rule = Validation.Rules.Value.build(field, predicate)
+    key_rule = Validation.Rules.RequiredKey.build(field)
 
-      [key_rule, value_rule]
-    end
+    [key_rule, value_rule]
   end
 
-  defmacro parse_predicate({:filled?, _, nil}) do
-    quote do
-      Validation.Predicates.Filled.build
-    end
+  defp parse_predicate({:filled?, _, nil}) do
+    Validation.Predicates.Filled.build
   end
 end
